f51d2ae7f14a088f97590d7ae9039e0a0095b513
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-05-23 06:01:08 +0900

    fix : create, deleteScrap 수정

--- MyRealTrip/src/main/java/sopt/org/MyRealTrip/infrastructure/ScrapRepository.java
+++ MyRealTrip/src/main/java/sopt/org/MyRealTrip/infrastructure/ScrapRepository.java
@@ -1,5 +1,6 @@
 package sopt.org.MyRealTrip.infrastructure;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import sopt.org.MyRealTrip.domain.Scrap;
 
@@ -9,6 +10,8 @@ public interface ScrapRepository extends Repository<Scrap, Long> {
     Scrap save(Scrap scrap);
 
     Optional<Long> findById(Long scrapId);
+    @Query("select s from Scrap s where s.user.id = :userId and s.tour.id = :tourId")
+    Scrap findByUserIdAndTourId(Long userId, Long tourId);
 
     Long deleteById(Long scrapId);
 }
